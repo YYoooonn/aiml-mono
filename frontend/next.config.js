9604d235f3c8349c58516ebb0afc64794855d7f1
Jonguk, Yoon <89994165+YYoooonn@users.noreply.github.com>
2024-10-02 03:09:34 +0900

    fix: fix lint

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: 'standalone',  // 여기서 standalone 폴더를 사용한다고 해준다.
+  output: "standalone", // 여기서 standalone 폴더를 사용한다고 해준다.
 };
 
-module.exports = nextConfig
+module.exports = nextConfig;

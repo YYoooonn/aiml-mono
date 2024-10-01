46df602ac4441714be532bd2a70c6bbda9620a1b
Jonguk, Yoon <89994165+YYoooonn@users.noreply.github.com>
2024-10-02 02:34:17 +0900

    fix: fix dockerfile

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,8 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: { appDir: true }, // Next.js v13 app directory 사용 여부
   output: 'standalone',  // 여기서 standalone 폴더를 사용한다고 해준다.
 };
 
-export default nextConfig;
+module.exports = nextConfig

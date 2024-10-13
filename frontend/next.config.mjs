6a012451eb9f4db2e5f214f61879c3a220d26610
Jonguk, Yoon <89994165+YYoooonn@users.noreply.github.com>
2024-10-13 19:25:55 +0900

    feat: socket setup

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,7 +1,12 @@
-/** @type {import('next').NextConfig} */
+// @ts-check
+
+/**
+ * @type {import('next').NextConfig}
+ */
 const nextConfig = {
+  /* config options here */
   reactStrictMode: true,
   output: "standalone", // 여기서 standalone 폴더를 사용한다고 해준다.
 };
 
-module.exports = nextConfig;
+export default nextConfig;

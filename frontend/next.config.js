d96b23ea7b11c4ee7747b83b685163161c38f273
Jonguk, Yoon <89994165+YYoooonn@users.noreply.github.com>
2024-10-20 01:16:11 +0900

    feat: websocket initialize

--- frontend/next.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  output: "standalone", // 여기서 standalone 폴더를 사용한다고 해준다.
-};
-
-module.exports = nextConfig;
---
+++ frontend/server.ts
@@ -0,0 +1,41 @@
+import { createServer } from "node:http";
+import next from "next";
+import { Server } from "socket.io";
+import dotenv from "dotenv";
+
+const dev = process.env.NODE_ENV !== "production";
+
+dotenv.config(dev ? { path: ".env.local" } : { path: ".env.production.local" });
+
+const hostname = dev? "localhost" : process.env.PUBLIC_URL;
+const port = dev? 3000 : Number(process.env.PORT || 3000);
+// when using middleware `hostname` and `port` must be provided below
+const app = next({ dev, hostname, port });
+const handler = app.getRequestHandler();
+
+app.prepare().then(() => {
+  const httpServer = createServer(handler);
+
+  const io = new Server(httpServer, {
+    path: "/socket.io/",
+    cors: {
+      origin: [`http://${hostname}`, `http://${hostname}:${port}`],
+      methods: ["GET", "POST"],
+    },
+    // addTrailingSlash: true
+  });
+
+  io.on("connection", (socket) => {
+    // ...
+    console.log(socket.id);
+  });
+
+  httpServer
+    .once("error", (err) => {
+      console.error(err);
+      process.exit(1);
+    })
+    .listen(port, () => {
+      console.log(`> Ready on http://${hostname}:${port}`);
+    });
+});

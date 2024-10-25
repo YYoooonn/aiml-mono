165e5dc33fb69d1a6b87c2a3fd590e0477639ef5
Jonguk, Yoon <89994165+YYoooonn@users.noreply.github.com>
2024-10-25 20:48:13 +0900

    feat: front/socket (#22)
    * feat: websocket initialize
    
    * fix: add url to socket client
    
    * fix: add url to socket client
    
    * fix: env
    
    * fix: nginx config and client side error catchc
    
    * fix: format lint
    
    * feat: test on chat

---
+++ next.config.mjs
@@ -0,0 +1,13 @@
+// @ts-check
+
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+  /* config options here */
+  reactStrictMode: true,
+  swcMinify: true,
+  // output: "standalone", // 여기서 standalone 폴더를 사용한다고 해준다.
+};
+
+export default nextConfig;

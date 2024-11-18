390686d4746ef52aee745b2b668d0dbabd5e3828
JunYoung An <anjnyng98@gmail.com>
2024-11-18 19:55:35 +0900

    add input for Email

--- backend/src/main/java/com/AIMLproject/backend/dto/req/RegisterReq.java
+++ backend/src/main/java/com/AIMLproject/backend/dto/req/RegisterReq.java
@@ -5,7 +5,8 @@ import lombok.Getter;
 @Getter
 public class RegisterReq {
 	private String username;
+	private String password;
 	private String firstName;
 	private String lastName;
-	private String password;
+	private String email;
 }

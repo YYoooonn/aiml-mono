dbb7ef088228e165bf731c6a45a417efac767b87
JunYoung An <anjnyng98@gmail.com>
2024-11-18 19:48:58 +0900

    Fix Authorized URL

--- backend/src/main/java/com/AIMLproject/backend/config/SecurityConfig.java
+++ backend/src/main/java/com/AIMLproject/backend/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.AIMLproject.backend.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -33,7 +34,9 @@ public class SecurityConfig {
 		httpSecurity
 			.csrf(AbstractHttpConfigurer::disable)
 			.authorizeHttpRequests(authz -> authz
-				.requestMatchers("/api/auth/**", "/api/users/register/**", "/api/test/**", "/api/projects/**")
+				.requestMatchers(HttpMethod.GET, "/api/projects/{projectId}")
+				.permitAll()
+				.requestMatchers("/api/auth/login", "/api/users/register")
 				.permitAll()
 				.anyRequest()
 				.authenticated()

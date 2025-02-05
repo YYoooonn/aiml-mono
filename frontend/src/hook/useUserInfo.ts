aa497ec3059487f20671f82b8d14a5bb3ca66bbf
Jonguk, Yoon <89994165+YYoooonn@users.noreply.github.com>
2025-02-05 17:39:09 +0900

    feat: add logout button

--- frontend/src/hook/useUserInfo.ts
+++ frontend/src/hook/useUserInfo.ts
@@ -4,12 +4,14 @@ import { create } from "zustand";
 import { persist } from "zustand/middleware";
 import { UserInfo, Project } from "@/@types/api";
 import { fetchUserInfo, updateUserInfo, UpdateInfo } from "@/app/_actions/user";
+import { deleteCookie } from "@/app/_actions/auth";
 
 interface UserActions {
   // setUser: (user: UserStoreState) => void;
   setAll: (data: UserInfo) => void;
   addProject: (project: Project) => void;
   reset: () => void;
+  logout: () => void;
   fetch: (username: string) => Promise<void>;
   update: (data: UpdateInfo) => Promise<void>;
   getProjects: () => Project[];
@@ -35,6 +37,10 @@ export const useUserInfo = create<UserState>()((set, get) => ({
   getProjects: () => get().projects,
   addProject: (project) => set({ projects: [...get().projects, project] }),
   reset: () => set(DEFAULT),
+  logout: () => {
+    deleteCookie();
+    set(DEFAULT);
+  },
   fetch: async (username) => {
     set(DEFAULT);
     const response = await fetchUserInfo(username);

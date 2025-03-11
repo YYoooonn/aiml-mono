5b7b7d2e5073ef91c2fbbb0fd90ecf0c66467f23
Jonguk, Yoon <89994165+YYoooonn@users.noreply.github.com>
2025-03-11 20:47:15 +0900

    refactor: unused editor props clean up

--- frontend/src/app/user/[username]/projects/[projectId]/page.css.ts
+++
@@ -1,9 +0,0 @@
-import { style } from "@vanilla-extract/css";
-import { breakpoints } from "@/styles/breakpoints";
-import { theme } from "@/styles/theme.css";
-import * as constants from "@/styles/constants";
-
-export const projectPageContainer = style({
-  height: `calc(100vh - ${constants.HEADERHEIGHT} - ${constants.FOOTERHEIGHT})`,
-  width: "100%",
-});

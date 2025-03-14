ae21526a959cc6de9a147c36b320f786002837da
Jonguk, Yoon <89994165+YYoooonn@users.noreply.github.com>
2025-03-14 21:24:53 +0900

    trim for backend

--- front/src/components/components.tsx
+++
@@ -1,5 +0,0 @@
-import Header from "./header/header";
-import Footer from "./footer/Footer";
-import { LeftAisle, RightAisle } from "./aisle/aisle";
-
-export { Header, Footer, LeftAisle, RightAisle };
--- front/src/components/footer/footer.tsx
+++
@@ -1,5 +0,0 @@
-import * as styles from "./footer.css";
-
-export default function Footer() {
-  return <div className={styles.footerContainer}>Footer</div>;
-}
